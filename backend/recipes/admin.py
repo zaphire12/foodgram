e726883e8e6596b2185c729289532e9413d39a08
NebievNI <nebiev.roma@yandex.ru>
2024-11-21 19:00:34 +0300

    admin zone

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from core.constants import INGREDIENT_MIN_AMOUNT
-from recipes.models import Ingredient, RecipeIngredient
+from recipes.models import Ingredient, RecipeIngredient, Tag, Favorite, ShoppingCart, Recipe
 
 
 class RecipeIngredientInline(admin.TabularInline):
-    """Строчное представление ингредиента в рецепте."""
-
     model = RecipeIngredient
     min_num = INGREDIENT_MIN_AMOUNT
 
@@ -17,3 +16,65 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display_links = ('name',)
     search_fields = ('name',)
     search_help_text = 'Поиск по названию ингредиента'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name', 'slug')
+
+
+@admin.register(Favorite, ShoppingCart)
+class AuthorRecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    list_editable = ('user', 'recipe')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Админка Рецептов."""
+
+    list_display = ('name', 'author', 'in_favorites',
+                    'get_ingredients', 'get_tags', 'image_tag')
+    list_display_links = ('name', 'author')
+    search_fields = ('name', 'author__username')
+    search_help_text = 'Поиск по названию рецепта или по автору'
+    filter_horizontal = ('tags',)
+    list_filter = ('tags',)
+    empty_value_display = 'Не задано'
+    inlines = (RecipeIngredientInline,)
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'author',
+                    ('name', 'cooking_time'),
+                    'text',
+                    'image',
+                    'tags',
+                )
+            },
+        ),
+    )
+
+    @admin.display(description='В избранном')
+    def in_favorites(self, recipe):
+        return recipe.favorites.count()
+
+    @admin.display(description='Ингредиенты')
+    def get_ingredients(self, recipe):
+        return ', '.join([
+            f"{ingredient.name} ({ingredient.measurement_unit})"
+            for ingredient in recipe.ingredients.all()
+        ])
+
+    @admin.display(description='Теги')
+    def get_tags(self, recipe):
+        return ", ".join([tag.name for tag in recipe.tags.all()])
+
+    @admin.display(description='Картинка')
+    def image_tag(self, recipe):
+        return mark_safe(
+            f'<img src="{recipe.image.url}" width="80" height="60" />'
+        )
