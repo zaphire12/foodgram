8a50b1bdcfa9c5f00bb41245dc0d92b456c588be
NebievNI <nebiev.roma@yandex.ru>
2024-11-21 21:42:30 +0300

    fix2

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -41,7 +41,7 @@ const About = ({ updateOrders, orders }) => {
               Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="https://github.com/zaphire12" className={styles.textLink}>Небиев Нариман</a>
+              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
             </p>
           </div>
         </aside>
